c115397f2e55486afab279ae53088168ba557773
Areshka90 <anetta_05@mail.ru>
2025-03-27 14:07:55 +0300

    Update Balance.py

--- models/Balance.py
+++ models/Balance.py
@@ -1,1 +1,23 @@
+class Balance:
+    def __init__(self, transaction_history: TransactionHistory):
+        self.amount: float = 0.0
+        self.transaction_history = transaction_history
+
+    def add_funds(self, amount: float) -> None:
+        if amount <= 0:
+            raise ValueError("Сумма должна быть положительной")
+        self.amount += amount
+        self.transaction_history.add_transaction(
+            Transaction(amount, TransactionType.DEPOSIT)
+        )
+
+    def deduct_funds(self, amount: float) -> None:
+        if amount <= 0:
+            raise ValueError("Сумма должна быть положительной")
+        if self.amount < amount:
+            raise ValueError("Недостаточно средств")
+        self.amount -= amount
+        self.transaction_history.add_transaction(
+            Transaction(amount, TransactionType.WITHDRAWAL)
+        )
 

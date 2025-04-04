e26dc495dbb2c86e46dd891d4bf9de7eb8c922c7
Areshka90 <anetta_05@mail.ru>
2025-04-04 13:23:50 +0300

    Create test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,33 @@
+from app.database.config import get_db_settings
+from app.database.database import get_session, init_db, engine
+from database.services.crud.user import get_all_users, create_user
+from sqlmodel import Session
+from app.models.User import User
+
+if __name__ == "__main__":
+    test_user = User(email='test@mail.ru', password='test')
+    test_user_2 = User(email='test@mail.ru', password='test')
+    test_user_3 = User(email='test@mail.ru', password='test')
+
+    settings = get_db_settings()
+    print(settings.DB_HOST)
+    print(settings.DB_NAME)
+
+    init_db()
+    print('Init db has been success')
+
+    with Session(engine) as session:
+        create_user(test_user, session)
+        create_user(test_user_2, session)
+        create_user(test_user_3, session)
+        users = get_all_users(session)
+
+    print('-------------------')
+    print(id(test_user) == id(users[0]))
+    print(id(test_user))
+    print(id(users[0]))
+
+    for user in users:
+        print(f'id: {user.id} - {user.email}')
+        print(type(user))
+        print(user.say[0])

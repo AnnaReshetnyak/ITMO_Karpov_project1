ec1ee9a9c7e761b2f3b8fa2ff3762b8de70f1a15
Areshka90 <anetta_05@mail.ru>
2025-04-15 10:31:39 +0300

    Update prediction.py

--- app/database/services/crud/prediction.py
+++ app/database/services/crud/prediction.py
@@ -1,26 +1,16 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
-from lesson_2.app.database.models import MLTask
-
+from lesson_2.app.models.task import MLTask
+from lesson_2.app.schemas import TaskStatus
+from datetime import datetime
 
 class PredictionCRUD:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create_task(self, user_id: str, input_data: dict) -> MLTask:
-        task = MLTask(
-            user_id=user_id,
-            input_data=input_data,
-            status="pending"
-        )
-        self.session.add(task)
-        await self.session.commit()
-        await self.session.refresh(task)
-        return task
-
     async def update_task_status(
             self,
             task_id: str,
-            status: str,
+            status: TaskStatus,  # Используем Enum
             result: dict = None
     ) -> MLTask:
         task = await self.session.get(MLTask, task_id)
@@ -28,12 +18,11 @@ class PredictionCRUD:
             return None
 
         task.status = status
+        task.updated_at = datetime.utcnow()
+
         if result:
             task.result = result
 
         self.session.add(task)
         await self.session.commit()
         return task
-
-    async def get_task(self, task_id: str) -> MLTask:
-        return await self.session.get(MLTask, task_id)

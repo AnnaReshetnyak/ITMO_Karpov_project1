81095836364b7d3a416039a66da9c81bffe42692
Areshka90 <anetta_05@mail.ru>
2025-04-15 10:30:23 +0300

    Update task.py

--- models/task.py
+++ models/task.py
@@ -1,16 +1,19 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Column
+from sqlalchemy import Enum as SQLEnum
 from datetime import datetime
 from uuid import UUID, uuid4
 from typing import Optional
-
+from lesson_2.app.schemas import TaskStatus
 
 class MLTaskBase(SQLModel):
     user_id: UUID = Field(foreign_key="users.id")
     input_data: dict = Field(sa_column=Column(JSON))
-    status: str = Field(default="pending", index=True)
+    status: TaskStatus = Field(
+        default=TaskStatus.PENDING,
+        sa_column=Column(SQLEnum(TaskStatus)),
+        index=True
+    )
     result: Optional[dict] = None
-    model_version: str = Field(default="1.0", max_length=20)
-
 
 class MLTask(MLTaskBase, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)

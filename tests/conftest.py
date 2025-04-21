6bca60d049bbde4702b301c0d081b748c8376c76
Areshka90 <anetta_05@mail.ru>
2025-04-21 08:37:39 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,31 @@
 import pytest
-from sqlmodel import SQLModel, create_engine, Session
-from tests.demo_data import create_demo_users
+from fastapi.testclient import TestClient
+from app.main import app
+from lesson_2.app.database.database import get_session, init_db
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
-@pytest.fixture(scope="module")
-def test_engine():
-    return create_engine("sqlite:///test.db")
 
-@pytest.fixture(scope="function")
-def test_session(test_engine):
-    SQLModel.metadata.create_all(test_engine)
-    with Session(test_engine) as session:
+@pytest.fixture(name="db_session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
         yield session
-    SQLModel.metadata.drop_all(test_engine)
 
-@pytest.fixture(scope="function")
-def demo_session(test_engine):
-    SQLModel.metadata.create_all(test_engine)
-    with Session(test_engine) as session:
-        create_demo_users(session)
-        yield session
-    SQLModel.metadata.drop_all(test_engine)
+
+@pytest.fixture(name="client")
+def client_fixture(db_session: Session):
+    def get_session_override():
+        return db_session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()

d9a644d2accc95d387463c8119217b1b4e05c1f7
Areshka90 <anetta_05@mail.ru>
2025-04-21 12:13:23 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
-from app.main import app
+from lesson_2.main import app
 from lesson_2.app.database.database import get_session, init_db
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
+from sqlmodel import SQLModel, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from lesson_2.app.database.database import engine
+
 
 
 @pytest.fixture(name="db_session")
@@ -29,3 +33,16 @@ def client_fixture(db_session: Session):
     client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+@pytest.fixture(scope="function")
+async def db_session():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+        async with AsyncSession(conn) as session:
+            yield session
+            await session.rollback()
+    await engine.dispose()

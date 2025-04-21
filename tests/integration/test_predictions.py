4da719d22c69767a746f6485f3b8e430690a18c8
Areshka90 <anetta_05@mail.ru>
2025-04-21 19:16:40 +0300

    Update test_predictions.py

--- tests/integration/test_predictions.py
+++ tests/integration/test_predictions.py
@@ -2,6 +2,8 @@ import pytest
 from fastapi.testclient import TestClient
 from lesson_2.main import app
 from lesson_2.app.database.database import init_db
+from unittest.mock import AsyncMock, patch
+
 
 client = TestClient(app)
 
@@ -40,3 +42,13 @@ def test_prediction_flow():
 
     assert prediction.status_code == 200
     assert "task_id" in prediction.json()
+
+async def test_prediction_flow_with_mocked_rabbitmq(test_client, auth_headers):
+    with patch("app.rabbitmq.producer.send_prediction_task", new=AsyncMock()) as mock_send:
+        response = test_client.post(
+            "/predict",
+            headers=auth_headers,
+            json={"input_data": {"param1": 1.2}}
+        )
+        mock_send.assert_awaited_once()
+        assert response.json()["status"] == "pending"
